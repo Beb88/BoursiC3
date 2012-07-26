958eee9d1e08852a9399f39bf89cddfcaa5b028e
Bertrand louis <Beb@BCC.(none)>
2012-07-26 21:33:49 +0200

    Initial Commit

---
+++ BoursiC3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BoursiC3
+//
+//  Created by bertrand louis on 26/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BoursiC3/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  BoursiC3
+//
+//  Created by bertrand louis on 26/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
